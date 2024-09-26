abdcba491ec2d8f6f066004f39c763055109cfa3
emmkro <emmkro167@ihmeducation.se>
2024-09-26 11:47:37 +0200

    Cookie result

--- A1/js/app.js
+++ A1/js/app.js
@@ -71,9 +71,9 @@ function showText(){
 
 //COOKIE
 function saveAsCookie(){
-  document.cookie = "playerScore=" + playerResult + "; expires = Thu, 18 Dec 2024 12:00:00 UTC";
-  document.cookie = "computerScore=" + computerResult + "; expires = Thu, 18 Dec 2024 12:00:00 UTC";
-  document.cookie = "drawResult=" + drawResult + "; expires = Thu, 18 Dec 2024 12:00:00 UTC";
+  document.cookie = "playerScore=" + playerResult + "; expires = Thu, 18 Dec 2064 12:00:00 UTC";
+  document.cookie = "computerScore=" + computerResult + "; expires = Thu, 18 Dec 2064 12:00:00 UTC";
+  document.cookie = "drawResult=" + drawResult + "; expires = Thu, 18 Dec 2064 12:00:00 UTC";
   console.log (" ")
 
 }
